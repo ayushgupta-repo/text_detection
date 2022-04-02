b6869a3bb5bac4b3c067ab6545c24dc7905284db
ayushgupta-repo <19ce037@charusat.edu.in>
2022-04-03 01:46:48 +0530

    Creating boxes around characters in the image

--- textDetection.py
+++ textDetection.py
@@ -12,7 +12,25 @@ img = cv2.imread('t1.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # fetching text from the image
-print(pytesseract.image_to_string(img))
+# print(pytesseract.image_to_string(img))
+
+# detecting each character as per the sequence x, y, w, h (w and h are the diagonal points of the bounding box not actual width and height)
+# print(pytesseract.image_to_boxes(img))
+
+# getting image height and width
+hImg, wImg, _ = img.shape
+boxes = pytesseract.image_to_boxes(img)
+
+for b in boxes.splitlines():
+    # print(b)
+    b = b.split(' ')
+    print(b)
+
+    # extracting informations
+    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+
+    # image, (x, hImg-y), (width, hImg-y), (RGB color), thickness
+    cv2.rectangle(img, (x, hImg-y), (w, hImg-h), (0, 0, 255), 1)
 
 # showing image and creating wait key to infinity until close button is clicked
 cv2.imshow('Result', img)
